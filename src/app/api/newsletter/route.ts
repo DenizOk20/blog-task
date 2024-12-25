f48d1a0acb25d1aed307aef541115d7ef42333e7
Deniz Ök <deniz.ok2019@gmail.com>
2024-12-25 23:01:02 +0300

    fixed newsletter db

--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import { db } from "../../../../lib/db";
 
 export const POST = async (req: Request) => {
   try {
     const body = await req.json();
 
-    const mail = await prisma?.newsletter.create({
+    const mail = await db.newsletter.create({
       data: { email: body.email },
     });
     return NextResponse.json(mail, { status: 200 });
